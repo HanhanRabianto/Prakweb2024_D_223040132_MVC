cfc1b374472ed7ec67d90a3bb9163da315e5baa6
Hanhan32 <125179054+HanhanRabianto@users.noreply.github.com>
2024-11-02 11:04:16 +0700

    Membuat Aplikasi MVC dengan PHP #6 Assets
    Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-
-      <h1>About me</h1>
+<div class="container">
+      <h1 class="mt-4">About me</h1>
+      <img src="<?=BASEURL; ?>/img/profile.jpg" alt="Sandhika Galih"
+      widht="200" class="rounded-circle shadow">
       <p>Halo, nama saya <?= $data['nama']?> </p>
+</div>
